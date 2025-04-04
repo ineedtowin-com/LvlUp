ef5ccfef6457de2c62a105db4cc8df3f5fd1eaff
ineedtowin-com <dmpersonall4@gmail.com>
2025-04-04 12:19:12 +0300

    Update script.js
    troubleshooting.

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-function toggleFeature(featureNumber) {
-  const feature = document.getElementById(`feature${featureNumber}`);
-
+function toggleDescription(featureId) {
+  var feature = document.getElementById(featureId);
   if (feature.style.display === "none") {
     feature.style.display = "block";
   } else {
     feature.style.display = "none";
+  }
+}
